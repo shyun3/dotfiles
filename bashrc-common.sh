b8a4d5378abb9480cda0ebdad8c177a6606343fc
Mo Hyun <shyun3@yasld.com>
2024-10-07 16:25:47 -0700

    Use 40% fzf height in fbr() instead of fzf-tmux

--- bashrc-common.sh
+++ bashrc-common.sh
@@ -20,6 +20,6 @@ fbr() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
   branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+           fzf --height 40% -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
