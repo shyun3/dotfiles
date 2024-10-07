6e88f26dd65a705e9ab04c47f692dbe7a85f75a3
Mo Hyun <shyun3@yasld.com>
2024-10-07 16:25:48 -0700

    Add Bash alias for changing directory to the Git repo root

--- bashrc-common.sh
+++ bashrc-common.sh
@@ -1,22 +1,35 @@
+#######################################################################
+# Aliases
+alias cdgitroot='cd "$(git rev-parse --show-cdup)"'
+
+#######################################################################
+# Environment variables
 export EDITOR=nvim
 export VISUAL=nvim
 
 export BAT_THEME=OneHalfDark
 
+#######################################################################
+# Shell variables
 HISTSIZE=20000
 HISTFILESIZE=20000
 
+#######################################################################
+# Prompt
 _RED='\[$(tput setaf 1)\]'
 _RESET='\[$(tput sgr0)\]'
 
 # Add red git branch in front of prompt
 PS1="$_RED\$(__git_ps1 '[%s] ')$_RESET$PS1"
 
+#######################################################################
+# Tilix
 # See https://github.com/gnunn1/tilix/wiki/VTE-Configuration-Issue
 if [[ $TILIX_ID ]]; then
     source /etc/profile.d/vte.sh
 fi
 
+#######################################################################
 # fzf
 fbr() {
   local branches branch
