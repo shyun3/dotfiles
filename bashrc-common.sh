1cddd746fd6b07b8a0180b507599a91faa63088c
Mo Hyun <shyun3@yasld.com>
2024-10-07 16:25:47 -0700

    Add .bashrc and .gitconfig files

---
+++ bashrc-common.sh
@@ -0,0 +1,16 @@
+export EDITOR=nvim
+export VISUAL=nvim
+
+HISTSIZE=20000
+HISTFILESIZE=20000
+
+_RED='$(tput setaf 1)'
+_RESET='$(tput sgr0)'
+
+# Add red git branch in front of prompt
+PS1="$_RED\$(__git_ps1 '[%s] ')$_RESET$PS1"
+
+# See https://github.com/gnunn1/tilix/wiki/VTE-Configuration-Issue
+if [[ $TILIX_ID ]]; then
+    source /etc/profile.d/vte.sh
+fi
