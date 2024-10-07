0b020b08a8ff5d04382c897ce3f26a757dff3093
Mo Hyun <shyun3@yasld.com>
2024-10-07 16:26:03 -0700

    Use Oh My Posh and Terminal-Icons with PowerShell

--- profile-common.ps1
+++ profile-common.ps1
@@ -1,17 +1,9 @@
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme paradox
+
+Import-Module -Name Terminal-Icons
+
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-function prompt
-{
-    # `poetry shell` does not change the prompt, so add the virtualenv name
-    $venvPrompt = $env:POETRY_ACTIVE -eq 1 -and $env:VIRTUAL_ENV ?
-        '(' + (Get-Item $env:VIRTUAL_ENV).BaseName + ') ' : ''
-
-    $defaultPrompt = 'PS ' +
-        $($executionContext.SessionState.Path.CurrentLocation) +
-        "$('>' * ($nestedPromptLevel + 1)) "
-
-    $venvPrompt + $defaultPrompt
-}
 
 # See https://stackoverflow.com/a/16949127
 function which($name)
