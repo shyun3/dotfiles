bf4659c150af171373056e83aaedb3d34b54afdf
Mo Hyun <shyun3@yasld.com>
2024-10-07 16:25:48 -0700

    Fix PowerShell prompt on newer Oh My Posh version
    Tested with 7.85.3.

--- profile-common.ps1
+++ profile-common.ps1
@@ -4,27 +4,27 @@
 
 Import-Module posh-git
 
-Import-Module oh-my-posh
 oh-my-posh init pwsh `
     --config 'https://raw.githubusercontent.com/shyun3/dotfiles/main/mytheme.omp.json' |
     Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
-$origPrompt = (Get-Command Prompt).ScriptBlock
-
 function prompt
 {
     # Make sure Windows Terminal duplicates new tabs/panes in same directory
     # https://docs.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#powershell-with-posh-git
     $loc = Get-Location
-    $cwdSeq = "$([char]27)]9;12$([char]7)"
+
+    $prompt = & (Get-Module oh-my-posh-core).ExportedCommands['prompt']
+
+    $prompt += "$([char]27)]9;12$([char]7)"
     if ($loc.Provider.Name -eq "FileSystem")
     {
-        $cwdSeq += "$([char]27)]9;9;`"$($loc.Path)`"$([char]7)"
+        $prompt += "$([char]27)]9;9;`"$($loc.Path)`"$([char]7)"
     }
 
-    ($origPrompt | Invoke-Expression) + $cwdSeq
+    $prompt
 }
 
 #######################################################################
