6f48335228f1281ab06ea770f492e3ab4317ed31
Mo Hyun <shyun3@yasld.com>
2024-10-07 16:25:48 -0700

    Duplicate PowerShell tabs/panes in same directory

--- profile-common.ps1
+++ profile-common.ps1
@@ -5,6 +5,21 @@ Import-Module -Name Terminal-Icons
 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
+$origPrompt = (Get-Command Prompt).ScriptBlock
+
+function prompt
+{
+    # https://docs.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#powershell-with-posh-git
+    $loc = Get-Location
+    $cwdSeq = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq "FileSystem")
+    {
+        $cwdSeq += "$([char]27)]9;9;`"$($loc.Path)`"$([char]7)"
+    }
+
+    ($origPrompt | Invoke-Expression) + $cwdSeq
+}
+
 # See https://stackoverflow.com/a/16949127
 function which($name)
 {
