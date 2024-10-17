e043dcea67604484ad3461dbb3de3de6588ac21b
Mo Hyun <shyun3@yasld.com>
2024-10-17 12:41:36 -0700

    Fix PowerShell setup

--- scripts/install_pkg.ps1
+++ scripts/install_pkg.ps1
@@ -1,5 +1,5 @@
 param ($cmd, $pkg)
 
-. "$PSScriptRoot/util.ps1"
+. $PSScriptRoot/util.ps1
 
 if (!(Check-Command $cmd)) { winget install --id $pkg --source winget --exact }
--- scripts/setup_ps.ps1
+++ scripts/setup_ps.ps1
@@ -1,11 +1,8 @@
-. "$PSScriptRoot/util.ps1"
+. $PSScriptRoot/util.ps1
 
-$PSScriptRoot/install_pkg.ps1 oh-my-posh JanDeDobbeleer.OhMyPosh
-
-###
+& $PSScriptRoot/install_pkg.ps1 oh-my-posh JanDeDobbeleer.OhMyPosh
 
 $modules = @("Terminal-Icons", "posh-git")
-
 foreach ($mod in $modules) {
     if (!(Check-Module $mod)) { Install-Module $mod }
 }
