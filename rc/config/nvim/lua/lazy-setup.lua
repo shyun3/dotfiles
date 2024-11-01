0a60a275ebcaabd6ea5717dd0444326bcff9e6ec
Mo Hyun <shyun3@yasld.com>
2024-11-01 16:11:20 -0700

    nvim: Exit on lazy clone fail

--- rc/config/nvim/lua/lazy-setup.lua
+++ rc/config/nvim/lua/lazy-setup.lua
@@ -1,7 +1,7 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
@@ -9,9 +9,19 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "--branch=stable", -- latest stable release
     lazypath,
   })
-end
 
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup("plugins", {
   install = { colorscheme = { "default" } },
   change_detection = { notify = false },
