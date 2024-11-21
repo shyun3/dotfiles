80f4aeb735f1fda2ba2e411846d55ca4a3001b00
Mo Hyun <shyun3@yasld.com>
2024-11-21 12:59:08 -0800

    Set Vim continuation indent width

---
+++ rc/config/nvim/after/ftplugin/vim.lua
@@ -0,0 +1,8 @@
+vim.opt_local.textwidth = 0
+
+local tabstop = 2
+vim.opt_local.tabstop = tabstop
+vim.opt_local.softtabstop = tabstop
+vim.opt_local.shiftwidth = tabstop
+
+vim.g.vim_indent_cont = tabstop
