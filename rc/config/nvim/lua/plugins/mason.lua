5e481216cf5702e64d7f87b6ef3ba618b675dd99
Mo Hyun <shyun3@yasld.com>
2024-12-12 14:01:05 -0800

    Update clang-format to 19.1.5

--- rc/config/nvim/lua/plugins/mason.lua
+++ rc/config/nvim/lua/plugins/mason.lua
@@ -34,7 +34,7 @@ return {
       ensure_installed = {
         -- Formatters
         "black",
-        { "clang-format", version = "19.1.4" },
+        { "clang-format", version = "19.1.5" },
         "isort",
         "prettier",
         "shfmt",
