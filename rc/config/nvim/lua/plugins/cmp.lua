add5043976cd42ee51cf5c83ca93e2fb2e433bda
Mo Hyun <shyun3@yasld.com>
2025-02-26 10:50:57 -0800

    cmp: Use all buffers as sources

--- rc/config/nvim/lua/plugins/cmp.lua
+++ rc/config/nvim/lua/plugins/cmp.lua
@@ -150,13 +150,9 @@ return {
         {
           name = "buffer",
           option = {
-            -- Visible buffers
             get_bufnrs = function()
-              local bufs = {}
-              for _, win in ipairs(vim.api.nvim_list_wins()) do
-                bufs[vim.api.nvim_win_get_buf(win)] = true
-              end
-              return vim.tbl_keys(bufs)
+              -- All buffers
+              return vim.api.nvim_list_bufs()
             end,
           },
         },
