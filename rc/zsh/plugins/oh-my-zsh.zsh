3a3ed74ad33f8a1b4627c3fc85ac1e9ff6317e97
Mo Hyun <shyun3@yasld.com>
2025-04-04 09:51:52 -0700

    omz: Fix upgrade check not running

--- rc/zsh/plugins/oh-my-zsh.zsh
+++ rc/zsh/plugins/oh-my-zsh.zsh
@@ -1,5 +1,12 @@
 if [[ -n "$ZSH" ]]; then
     # This is run in oh-my-zsh.sh, but use-omz does not run that or check for
     # upgrade it seems
-    source "$ZSH/tools/check_for_upgrade.sh"
+    #
+    # There is a check in this script for stdout attached to terminal or
+    # Powerlevel10k instant prompt. For some reason, this fails even though
+    # Powerlevel10k is being sourced beforehand. Maybe antidote also messes
+    # with the terminal redirect.
+    #
+    # Anyway, force the instant prompt variable to allow the script to proceed
+    POWERLEVEL9K_INSTANT_PROMPT=on source "$ZSH/tools/check_for_upgrade.sh"
 fi
