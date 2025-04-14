d6b4fa443d41ec6f4c837448c57b8a63ceb7d870
Mo Hyun <shyun3@yasld.com>
2025-04-14 16:36:17 -0700

    conform: Increase format on save timeout
    black was having timeout errors.

--- rc/config/nvim/lua/plugins/conform.lua
+++ rc/config/nvim/lua/plugins/conform.lua
@@ -55,7 +55,7 @@ return {
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 500, lsp_fallback = true, quiet = true }
+      return { timeout_ms = 1000, lsp_fallback = true, quiet = true }
     end,
   },
 
