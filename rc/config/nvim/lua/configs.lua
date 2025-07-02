af4c8faf65362eee4bb9a9b159a9c63fd618266a
Mo Hyun <shyun3@yasld.com>
2025-07-02 09:54:50 -0700

    nvim: Use icons for diagnostic signs

--- rc/config/nvim/lua/configs.lua
+++ rc/config/nvim/lua/configs.lua
@@ -1,1 +1,13 @@
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({
+  virtual_text = true,
+
+  signs = {
+    text = {
+      -- Taken from lualine diagnostics component default symbols
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.INFO] = "󰋽 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+    },
+  },
+})
