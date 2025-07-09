85c101c4e5d3154a831e8c665a866b13f1a7a486
Mo Hyun <shyun3@yasld.com>
2025-07-09 11:11:57 -0700

    lazy: Remove lazydev dependency
    No longer seems to be needed.

--- rc/config/nvim/lua/plugins/folke.lua
+++ rc/config/nvim/lua/plugins/folke.lua
@@ -114,13 +114,12 @@ return {
 
   {
     "folke/lazydev.nvim",
-    dependencies = { "Bilal2453/luvit-meta" },
     ft = "lua",
 
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
