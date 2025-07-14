b054d0d9f9d750a6f78c15e577c0c5424dbc7a40
Mo Hyun <shyun3@yasld.com>
2025-07-14 15:49:53 -0700

    luasnip: Move spec to separate file

--- rc/config/nvim/lua/plugins.lua
+++ rc/config/nvim/lua/plugins.lua
@@ -69,52 +69,6 @@ return {
   },
 
   {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    build = "make install_jsregexp",
-    lazy = true,
-
-    config = function()
-      for _, fmt in ipairs({ "vscode", "snipmate" }) do
-        local module = string.format("luasnip.loaders.from_%s", fmt)
-        require(module).lazy_load({
-          lazy_paths = { "./snippets/specific" },
-        })
-      end
-
-      -- Update repeated placeholders while typing in insert mode
-      -- Derived from https://github.com/L3MON4D3/LuaSnip/wiki/Migrating-from-UltiSnips#update-placeholders
-      local luasnip = require("luasnip")
-      luasnip.setup({
-        update_events = { "TextChanged", "TextChangedI" },
-      })
-
-      -- Derived from https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1500869758
-      local function stop_snippet()
-        -- If we have n active nodes, n - 1 will still remain after a `unlink_current()` call.
-        -- We unlink all of them by wrapping the calls in a loop.
-        while true do
-          if
-            luasnip.session
-            and luasnip.session.current_nodes[vim.fn.bufnr()]
-            and not luasnip.session.jump_active
-          then
-            luasnip.unlink_current()
-          else
-            break
-          end
-        end
-      end
-
-      -- Derived from https://github.com/Saghen/blink.cmp/issues/1805#issuecomment-2912427954
-      vim.keymap.set({ "n", "i", "s" }, "<Esc>", function()
-        stop_snippet()
-        return "<Esc>"
-      end, { silent = true, expr = true, desc = "Escape and stop snippet" })
-    end,
-  },
-
-  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {},
