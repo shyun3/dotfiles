b4a5168d341293b800f78c68559b9465056e1902
Mo Hyun <shyun3@yasld.com>
2025-07-16 15:13:35 -0700

    treesitter: Add move keys

--- rc/config/nvim/lua/plugins/treesitter.lua
+++ rc/config/nvim/lua/plugins/treesitter.lua
@@ -89,6 +89,31 @@ return {
           },
         },
       },
+
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+
+        goto_next_start = {
+          ["]m"] = { query = "@function.outer", desc = "Next function start" },
+          ["]]"] = { query = "@class.outer", desc = "Next class start" },
+        },
+        goto_next_end = {
+          ["]M"] = { query = "@function.outer", desc = "Next function end" },
+          ["]["] = { query = "@class.outer", desc = "Next class end" },
+        },
+        goto_previous_start = {
+          ["[m"] = {
+            query = "@function.outer",
+            desc = "Previous function start",
+          },
+          ["[["] = { query = "@class.outer", desc = "Previous class start" },
+        },
+        goto_previous_end = {
+          ["[M"] = { query = "@function.outer", desc = "Previous function end" },
+          ["[]"] = { query = "@class.outer", desc = "Previous class end" },
+        },
+      },
     },
 
     endwise = { enable = true },
