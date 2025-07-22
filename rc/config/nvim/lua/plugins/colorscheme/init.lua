27661f91bf975abf88e5affa52d3051704f6ce59
Mo Hyun <shyun3@yasld.com>
2025-07-22 16:03:10 -0700

    colorscheme: Use catpucccin

--- rc/config/nvim/lua/plugins/colorscheme/init.lua
+++ rc/config/nvim/lua/plugins/colorscheme/init.lua
@@ -22,6 +22,11 @@ return {
         which_key = true,
       },
     },
+
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 
   {
