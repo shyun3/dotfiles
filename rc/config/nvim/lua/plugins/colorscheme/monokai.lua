98f50132f956a4fdc362e86934ba2ac7cef30ecc
Mo Hyun <shyun3@yasld.com>
2025-07-23 14:08:55 -0700

    lazy: Move monokai config to separate file

---
+++ rc/config/nvim/lua/plugins/colorscheme/monokai.lua
@@ -0,0 +1,19 @@
+return {
+  "polirritmico/monokai-nightasty.nvim",
+  lazy = false,
+  priority = 1000,
+
+  opts = {
+    -- Add headers marks highlights (the `#` character) to Treesitter highlight query
+    markdown_header_marks = true,
+
+    on_highlights = function(hl)
+      local dropbar = require("plugins.colorscheme.integrations").dropbar_custom
+      for group, hl_map in pairs(dropbar) do
+        hl[group] = hl_map
+      end
+    end,
+  },
+
+  config = function(_, opts) require("monokai-nightasty").load(opts) end,
+}
