23f7a76c5bc0bd512aa983408bc54d905e5e3523
Mo Hyun <shyun3@yasld.com>
2025-07-24 13:05:03 -0700

    cat: Move config to separate file

--- rc/config/nvim/lua/plugins/colorscheme/init.lua
+++ rc/config/nvim/lua/plugins/colorscheme/init.lua
@@ -1,50 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-
-    opts = {
-      styles = {
-        conditionals = {},
-      },
-
-      custom_highlights = function()
-        return vim.tbl_extend(
-          "error",
-          require("plugins.colorscheme.integrations").dropbar_overrides,
-          {
-            ["@function.builtin"] = { italic = true },
-
-            -- Treesitter seems to think anything with :: behind it is a module
-            ["@module.cpp"] = {},
-
-            ["@lsp.typemod.variable.readonly"] = { link = "@constant" },
-          }
-        )
-      end,
-
-      integrations = {
-        dropbar = {
-          enabled = true,
-          color_mode = true,
-        },
-        hop = true,
-        mason = true,
-        noice = true,
-        notify = true,
-        snacks = { enabled = true },
-        which_key = true,
-      },
-    },
-
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-
-  {
     "polirritmico/monokai-nightasty.nvim",
     lazy = false,
     priority = 1000,
@@ -79,5 +34,6 @@ return {
     },
   },
 
+  { import = "plugins.colorscheme.catppuccin" },
   { import = "plugins.colorscheme.molokai" },
 }
