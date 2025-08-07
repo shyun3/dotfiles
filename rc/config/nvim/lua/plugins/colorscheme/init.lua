c8a87c5ce6caab7cd2d4442cb75296c327b1a4e5
Mo Hyun <shyun3@yasld.com>
2025-08-07 10:58:06 -0700

    lazy: Remove monokai

--- rc/config/nvim/lua/plugins/colorscheme/init.lua
+++ rc/config/nvim/lua/plugins/colorscheme/init.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "polirritmico/monokai-nightasty.nvim",
-    lazy = false,
-    priority = 1000,
-
-    opts = {
-      -- Add headers marks highlights (the `#` character) to Treesitter highlight query
-      markdown_header_marks = true,
-
-      on_highlights = function(hl)
-        local dropbar =
-          require("plugins.colorscheme.integrations").dropbar_custom
-        for group, hl_map in pairs(dropbar) do
-          hl[group] = hl_map
-        end
-      end,
-    },
-  },
-
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
