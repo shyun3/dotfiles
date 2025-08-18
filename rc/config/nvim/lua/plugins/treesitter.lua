2e1cb66024069b4e7b7e9e722d52e6c4aa8911e5
Mo Hyun <shyun3@yasld.com>
2025-08-18 09:02:45 -0700

    treesitter: Enable highlighting for all languages

--- rc/config/nvim/lua/plugins/treesitter.lua
+++ rc/config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-local highlight_disable_langs = { "python" }
-
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
@@ -10,6 +8,8 @@ return {
 
   branch = "master",
   build = ":TSUpdate",
+
+  main = "nvim-treesitter.configs",
 
   opts = {
     -- A list of parser names, or "all"
@@ -46,8 +46,6 @@ return {
       enable = true,
 
       disable = function(lang, buf)
-        if vim.tbl_contains(highlight_disable_langs, lang) then return true end
-
         -- disable slow treesitter highlight for large files
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -112,27 +110,4 @@ return {
 
     endwise = { enable = true },
   },
-
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-
-    -- Disabling treesitter highlighting also disables text objects, see
-    -- nvim-treesitter-textobjects#746. Manually force treesitter evaluation
-    -- for highlight disabled languages as a workaround.
-    --
-    -- Derived from https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/746#issuecomment-2861663503
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("user_treesitter", {}),
-      desc = "Load treesitter parser and parse tree",
-      pattern = highlight_disable_langs,
-
-      callback = function(ev)
-        local parsers = require("nvim-treesitter.parsers")
-        local lang = parsers.get_buf_lang(ev.buf)
-        local parser =
-          vim.treesitter.get_parser(ev.buf, lang, { error = false })
-        if parser then parser:parse() end
-      end,
-    })
-  end,
 }
