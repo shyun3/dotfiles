456792d37df29ae57b340cf8bcf092c435bf67c4
Mo Hyun <shyun3@yasld.com>
2025-09-10 15:10:51 -0700

    mini.ai: Create fallbacks for treesitter specs

--- rc/config/nvim/lua/plugins/mini/init.lua
+++ rc/config/nvim/lua/plugins/mini/init.lua
@@ -9,16 +9,29 @@ return {
     config = function()
       local mini_ai = require("mini.ai")
 
-      local spec_treesitter = mini_ai.gen_spec.treesitter
+      local gen_spec = mini_ai.gen_spec
+      local spec_treesitter = gen_spec.treesitter
       local gen_ai_spec = require("mini.extra").gen_ai_spec
 
       mini_ai.setup({
         custom_textobjects = {
-          a = spec_treesitter({
-            a = "@parameter.outer",
-            i = "@parameter.inner",
-          }),
-          f = spec_treesitter({ a = "@call.outer", i = "@call.inner" }),
+          a = function(ai_type)
+            return require("nvim-treesitter.parsers").has_parser()
+                and spec_treesitter({
+                  a = "@parameter.outer",
+                  i = "@parameter.inner",
+                })(ai_type)
+              or gen_spec.argument()
+          end,
+
+          f = function(ai_type)
+            return require("nvim-treesitter.parsers").has_parser()
+                and spec_treesitter({ a = "@call.outer", i = "@call.inner" })(
+                  ai_type
+                )
+              or gen_spec.function_call()
+          end,
+
           k = spec_treesitter({ a = "@class.outer", i = "@class.inner" }),
           F = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
           [";"] = spec_treesitter({ a = "@block.outer", i = "@block.inner" }),
