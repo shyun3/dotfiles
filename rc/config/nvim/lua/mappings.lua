b7243a8465caba2011fafa6701256f87f05afcf1
Mo Hyun <shyun3@yasld.com>
2025-09-26 11:48:25 -0700

    nvim: Add mapping descriptions

--- rc/config/nvim/lua/mappings.lua
+++ rc/config/nvim/lua/mappings.lua
@@ -1,37 +1,118 @@
 -- List tags before jumping if more than one match
-vim.keymap.set({ "n", "v" }, "<C-]>", "g<C-]>")
+vim.keymap.set(
+  { "n", "v" },
+  "<C-]>",
+  "g<C-]>",
+  { desc = "Tag: Jump to definition" }
+)
 
 -- Open tags in splits
-vim.keymap.set("n", "<A-]>s", "<Cmd>wincmd g<C-]><CR>")
-vim.keymap.set("n", "<A-]>v", "<Cmd>vertical wincmd g<C-]><CR>")
+vim.keymap.set(
+  "n",
+  "<A-]>s",
+  "<Cmd>wincmd g<C-]><CR>",
+  { desc = "Tag: Open definition in horizontal split" }
+)
+vim.keymap.set(
+  "n",
+  "<A-]>v",
+  "<Cmd>vertical wincmd g<C-]><CR>",
+  { desc = "Tag: Open definition in vertical split" }
+)
 
 -- Buffers
-vim.keymap.set("n", "<Leader><BS>", "<Cmd>bdelete<CR>")
+vim.keymap.set(
+  "n",
+  "<Leader><BS>",
+  "<Cmd>bdelete<CR>",
+  { desc = "Unload current buffer" }
+)
 
 -- Window navigation
-vim.keymap.set("n", "]w", "<Cmd>wincmd w<CR>")
-vim.keymap.set("n", "[w", "<Cmd>wincmd W<CR>")
-vim.keymap.set("n", "<BS>", "<Cmd>wincmd p<CR>")
-vim.keymap.set("n", "<Left>", "<Cmd>wincmd h<CR>")
-vim.keymap.set("n", "<Down>", "<Cmd>wincmd j<CR>")
-vim.keymap.set("n", "<Up>", "<Cmd>wincmd k<CR>")
-vim.keymap.set("n", "<Right>", "<Cmd>wincmd l<CR>")
-vim.keymap.set("n", "<A-c>", "<Cmd>wincmd c<CR>")
-vim.keymap.set("n", "<A-o>", "<Cmd>wincmd o<CR>")
+vim.keymap.set(
+  "n",
+  "]w",
+  "<Cmd>wincmd w<CR>",
+  { desc = "Go to N next window (wrap around)" }
+)
+vim.keymap.set(
+  "n",
+  "[w",
+  "<Cmd>wincmd W<CR>",
+  { desc = "Go to N previous window (wrap around)" }
+)
+vim.keymap.set(
+  "n",
+  "<BS>",
+  "<Cmd>wincmd p<CR>",
+  { desc = "Go to last accessed window" }
+)
+vim.keymap.set(
+  "n",
+  "<Left>",
+  "<Cmd>wincmd h<CR>",
+  { desc = "Go to Nth left window (stop at first)" }
+)
+vim.keymap.set(
+  "n",
+  "<Down>",
+  "<Cmd>wincmd j<CR>",
+  { desc = "Go N windows down (stop at last)" }
+)
+vim.keymap.set(
+  "n",
+  "<Up>",
+  "<Cmd>wincmd k<CR>",
+  { desc = "Go N windows up (stop at first)" }
+)
+vim.keymap.set(
+  "n",
+  "<Right>",
+  "<Cmd>wincmd l<CR>",
+  { desc = "Go to Nth right window (stop at last)" }
+)
+vim.keymap.set(
+  "n",
+  "<A-c>",
+  "<Cmd>wincmd c<CR>",
+  { desc = "Close current window" }
+)
+vim.keymap.set(
+  "n",
+  "<A-o>",
+  "<Cmd>wincmd o<CR>",
+  { desc = "Close all but current window" }
+)
 for i = 1, 9 do
   vim.keymap.set(
     "n",
     string.format("<A-%d>", i),
-    string.format("<Cmd>%dwincmd w<CR>", i)
+    string.format("<Cmd>%dwincmd w<CR>", i),
+    { desc = "Go to window " .. i }
   )
 end
 
 -- Quickfix
-vim.keymap.set("n", "<A-q>", "<Cmd>botright copen<CR>")
-vim.keymap.set("n", "<Leader>qq", "<Cmd>cclose<CR>")
+vim.keymap.set(
+  "n",
+  "<A-q>",
+  "<Cmd>botright copen<CR>",
+  { desc = "Open quickfix window" }
+)
+vim.keymap.set(
+  "n",
+  "<Leader>qq",
+  "<Cmd>cclose<CR>",
+  { desc = "Close quickfix window" }
+)
 
 -- Help
-vim.keymap.set("n", "<Leader>qh", "<Cmd>helpclose<CR>")
+vim.keymap.set(
+  "n",
+  "<Leader>qh",
+  "<Cmd>helpclose<CR>",
+  { desc = "Close one help window" }
+)
 
 -- Highlights
 vim.keymap.set("n", "zS", vim.show_pos, { desc = "Inspect" })
