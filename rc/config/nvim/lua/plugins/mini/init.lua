b711cc1fd062be8f023c840ee0ad2d891fbc5840
Mo Hyun <shyun3@yasld.com>
2025-10-02 10:29:42 -0700

    lazy: Add mini.pairs

--- rc/config/nvim/lua/plugins/mini/init.lua
+++ rc/config/nvim/lua/plugins/mini/init.lua
@@ -1,4 +1,23 @@
 return {
+  {
+    "nvim-mini/mini.pairs",
+    version = false,
+    event = "CmdlineEnter",
+
+    opts = {
+      modes = {
+        insert = false, -- Handled by nvim-autopairs
+        command = true,
+      },
+
+      mappings = {
+        ["'"] = {
+          neigh_pattern = "[^\\].", -- Allow auto-pairing after letter
+        },
+      },
+    },
+  },
+
   { import = "plugins.mini.ai" },
   { import = "plugins.mini.files" },
 }
