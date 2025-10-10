44d493759adf85a90483fca4f441951d6a82052c
Mo Hyun <shyun3@yasld.com>
2025-10-10 09:39:34 -0700

    asterisk: Add remaining mappings

--- rc/config/nvim/lua/plugins.lua
+++ rc/config/nvim/lua/plugins.lua
@@ -143,10 +143,68 @@ return {
 
     keys = {
       {
+        "*",
+        "<Plug>(asterisk-*)",
+        desc = "Search forward for whole word",
+      },
+      {
+        "*",
+        "<Plug>(asterisk-*)",
+        mode = "x",
+        desc = "Search forward for selection",
+      },
+      {
+        "#",
+        "<Plug>(asterisk-#)",
+        desc = "Search backward for whole word",
+      },
+      {
+        "#",
+        "<Plug>(asterisk-#)",
+        mode = "x",
+        desc = "Search backward for selection",
+      },
+      {
+        "g*",
+        "<Plug>(asterisk-g*)",
+        desc = "Search forward for word",
+      },
+      {
+        "g#",
+        "<Plug>(asterisk-g#)",
+        desc = "Search backward for word",
+      },
+      {
         "z*",
         "<Plug>(asterisk-z*)",
-        mode = { "n", "x" },
-        desc = "Highlight all occurrences of word",
+        desc = "Highlight all occurrences of whole word, set direction forward",
+      },
+      {
+        "z*",
+        "<Plug>(asterisk-z*)",
+        mode = "x",
+        desc = "Highlight all occurrences of selection, set direction forward",
+      },
+      {
+        "gz*",
+        "<Plug>(asterisk-gz*)",
+        desc = "Highlight all occurrences of word, set direction forward",
+      },
+      {
+        "z#",
+        "<Plug>(asterisk-z#)",
+        desc = "Highlight all occurrences of whole word, set direction backward",
+      },
+      {
+        "z#",
+        "<Plug>(asterisk-z#)",
+        mode = "x",
+        desc = "Highlight all occurrences of selection, set direction backward",
+      },
+      {
+        "gz#",
+        "<Plug>(asterisk-gz#)",
+        desc = "Highlight all occurrences of word, set direction backward",
       },
     },
   },
