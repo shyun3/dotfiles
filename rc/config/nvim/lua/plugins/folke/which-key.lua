fdc550f47d9156be7f0ef865126381f352f8da1a
Mo Hyun <shyun3@yasld.com>
2025-10-15 10:14:31 -0700

    which-key: Add descriptions to built-ins

--- rc/config/nvim/lua/plugins/folke/which-key.lua
+++ rc/config/nvim/lua/plugins/folke/which-key.lua
@@ -2,22 +2,64 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
 
-  opts = { preset = "modern" },
+  opts = {
+    preset = "modern",
 
-  config = function(_, opts)
-    require("which-key").setup(opts)
+    spec = {
+      { "K", desc = "LSP: Hover" },
+      { "Y", desc = "Yank to end of line" },
+      { "&", desc = "Repeat last substitute with flags" },
+      { "<C-l>", desc = "Clear and redraw screen" },
 
-    for _, mode in pairs({ "i", "s", "x", "c", "o" }) do
-      vim.keymap.set(
-        mode,
-        "<C-q>",
-        function() require("which-key").show({ mode = mode }) end,
-        { desc = "which-key: Show mappings" }
-      )
-    end
-
-    require("which-key").add({
       { "gq", mode = { "n", "x" }, desc = "Custom format lines" },
+      { "gri", desc = "Implementations" },
+      { "grr", desc = "References" },
+      { "gO", desc = "Show outline" },
+
+      { "[a", desc = "Edit previous file in argument list" },
+      { "[b", desc = "Go to previous buffer in buffer list" },
+      { "[l", desc = "Location: Display previous error" },
+      { "[L", desc = "Location: Display first error" },
+      { "[q", desc = "Quickfix: Display previous error" },
+      { "[A", desc = "Edit first file in argument list" },
+      { "[B", desc = "Go to first buffer in buffer list" },
+      { "[Q", desc = "Quickfix: Display first error" },
+      { "[t", desc = "Jump to previous matching tag" },
+      { "[T", desc = "Jump to first matching tag" },
+      { "[<C-l>", desc = "Location: Display last error in previous file" },
+      { "[<C-q>", desc = "Quickfix: Display last error in previous file" },
+      { "[<C-t>", desc = "Jump to previous matching tag in preview window" },
+
+      { "]a", desc = "Edit next file in argument list" },
+      { "]b", desc = "Go to next buffer in buffer list" },
+      { "]l", desc = "Location: Display next error" },
+      { "]L", desc = "Location: Display last error" },
+      { "]A", desc = "Edit last file in argument list" },
+      { "]B", desc = "Go to last buffer in buffer list" },
+      { "]q", desc = "Quickfix: Display next error" },
+      { "]Q", desc = "Quickfix: Display last error" },
+      { "]t", desc = "Jump to next matching tag" },
+      { "]T", desc = "Jump to last matching tag" },
+      { "]<C-l>", desc = "Location: Display first error in next file" },
+      { "]<C-q>", desc = "Quickfix: Display first error in next file" },
+      { "]<C-t>", desc = "Jump to next matching tag in preview window" },
+
+      -- Insert mode
+      { "<C-u>", mode = "i", desc = "Delete characters before cursor" },
+      { "<C-w>", mode = "i", desc = "Delete word before cursor" },
+
+      -- Visual mode
+      {
+        "Q",
+        mode = "x",
+        desc = "Linewise only: Repeat last recorded register for each line",
+      },
+      {
+        "@",
+        mode = "x",
+        desc = "Linewise only: Execute register contents for each line",
+      },
+      { "a%", mode = "x", desc = "Matching group" },
 
       -- Open tags in splits
       { "<A-]>", desc = "Tag" },
@@ -44,7 +86,20 @@ return {
         "<Cmd>helpclose<CR>",
         desc = "Close one help window",
       },
-    })
+    },
+  },
+
+  config = function(_, opts)
+    require("which-key").setup(opts)
+
+    for _, mode in pairs({ "i", "s", "x", "c", "o" }) do
+      vim.keymap.set(
+        mode,
+        "<C-q>",
+        function() require("which-key").show({ mode = mode }) end,
+        { desc = "which-key: Show mappings" }
+      )
+    end
   end,
 
   keys = {
