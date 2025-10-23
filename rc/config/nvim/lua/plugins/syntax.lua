15e5e900f3ef241f102a31d60666b65deef0f4f8
Mo Hyun <shyun3@yasld.com>
2025-10-23 12:17:27 -0700

    polyglot: Remove faulty plug mapping

--- rc/config/nvim/lua/plugins/syntax.lua
+++ rc/config/nvim/lua/plugins/syntax.lua
@@ -11,6 +11,8 @@ return {
       vim.g.polyglot_disabled = { "autoindent", "ftdetect", "lua", "sensible" }
 
       -- vim-markdown
+      vim.g.vim_markdown_auto_insert_bullets = 0
+      vim.g.vim_markdown_new_list_item_indent = 0
       vim.g.vim_markdown_no_default_key_mappings = 1
     end,
 
@@ -18,20 +20,11 @@ return {
       -- polyglot sets this variable, which prevents loading of `filetype.lua`
       vim.g.did_load_filetypes = nil
 
-      ------------------------------------------------------------------------
       -- vim-cpp-modern
       vim.cmd.highlight("link cppSTLios NONE") -- STL I/O manipulators
       vim.cmd.highlight("link cppSTLconstant NONE") -- C++17 constants
       vim.cmd.highlight("link cppSTLtype NONE") -- C++17 STL types
       vim.cmd.highlight("link cppSTLconcept NONE") -- C++20 concepts
-
-      ------------------------------------------------------------------------
-      -- vim-markdown
-      vim.g.vim_markdown_auto_insert_bullets = 0
-      vim.g.vim_markdown_new_list_item_indent = 0
-
-      -- Disable `gx` mapping
-      vim.keymap.set("", "<Plug>", "<Plug>Markdown_OpenUrlUnderCursor")
     end,
   },
 
