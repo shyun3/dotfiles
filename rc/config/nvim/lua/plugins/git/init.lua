d9373fde994e4d64848ec37088f16b4857f0bf97
Mo Hyun <shyun3@yasld.com>
2025-10-23 15:44:18 -0700

    lazy: Create separate git module

---
+++ rc/config/nvim/lua/plugins/git/init.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufRead",
+
+    opts = {
+      -- Currently, this plugin is only being used for blame
+      signcolumn = false,
+      watch_gitdir = { enable = false },
+    },
+
+    keys = {
+      { "<Leader>gb", "<Cmd>Gitsigns blame<CR>", desc = "Git: Blame" },
+    },
+  },
+}
