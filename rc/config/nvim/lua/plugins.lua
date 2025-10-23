1ef95c771d81956545c5ffafa7d6c446334c9f9a
Mo Hyun <shyun3@yasld.com>
2025-10-23 15:37:56 -0700

    lazy: Add gitsigns

--- rc/config/nvim/lua/plugins.lua
+++ rc/config/nvim/lua/plugins.lua
@@ -21,6 +21,21 @@ return {
   },
 
   {
+    "lewis6991/gitsigns.nvim",
+    event = "BufRead",
+
+    opts = {
+      -- Currently, this plugin is only being used for blame
+      signcolumn = false,
+      watch_gitdir = { enable = false },
+    },
+
+    keys = {
+      { "<Leader>gb", "<Cmd>Gitsigns blame<CR>", desc = "Git: Blame" },
+    },
+  },
+
+  {
     "chrishrb/gx.nvim",
 
     init = function() vim.g.netrw_nogx = 1 end,
