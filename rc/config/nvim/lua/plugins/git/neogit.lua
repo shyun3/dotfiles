bde4e1b95dbe842463a9a6f1a4a3302451f1f89b
Mo Hyun <shyun3@yasld.com>
2025-10-24 14:24:37 -0700

    neogit: Add log keymap

--- rc/config/nvim/lua/plugins/git/neogit.lua
+++ rc/config/nvim/lua/plugins/git/neogit.lua
@@ -1,5 +1,15 @@
 return {
   { "nvim-lua/plenary.nvim", lazy = true },
 
-  { "NeogitOrg/neogit", config = true },
+  {
+    "NeogitOrg/neogit",
+
+    config = true,
+
+    cmd = "Neogit",
+
+    keys = {
+      { "<Leader>gl", "<Cmd>NeogitLog<CR>", desc = "Git: Log" },
+    },
+  },
 }
