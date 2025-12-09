751bf1e651f8115af844ac4486a59f9d4ea624bb
Mo Hyun <shyun3@yasld.com>
2025-12-09 14:03:46 -0800

    lazy: Add spider

--- rc/config/nvim/lua/plugins/wordmotion.lua
+++ rc/config/nvim/lua/plugins/wordmotion.lua
@@ -18,31 +18,6 @@ return {
 
     keys = {
       {
-        "<Leader>w",
-        "<Plug>WordMotion_w",
-        mode = { "n", "x", "o" },
-        desc = "Next subword",
-      },
-      {
-        "<Leader>e",
-        "<Plug>WordMotion_e",
-        mode = { "n", "x", "o" },
-        desc = "Next end of subword",
-      },
-      {
-        "<Leader>b",
-        "<Plug>WordMotion_b",
-        mode = { "n", "x", "o" },
-        desc = "Previous subword",
-      },
-      {
-        "<Leader>ge",
-        "<Plug>WordMotion_ge",
-        mode = { "n", "x", "o" },
-        desc = "Previous end of subword",
-      },
-
-      {
         "i<Leader>w",
         "<Plug>WordMotion_iw",
         mode = { "o", "x" },
