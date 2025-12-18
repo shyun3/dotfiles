9b45e6d4aed703b6eaa6452dcd814107b6bc0009
Mo Hyun <shyun3@yasld.com>
2025-12-18 10:22:43 -0800

    refactor(text-objects): which-key config

--- rc/config/nvim/lua/plugins/text-objects/init.lua
+++ rc/config/nvim/lua/plugins/text-objects/init.lua
@@ -1,18 +1,18 @@
 return {
   {
-    "glts/vim-textobj-comment",
-    dependencies = "kana/vim-textobj-user",
+    LazyDep("which-key"),
+    optional = true,
 
-    event = "ModeChanged",
+    opts = function(_, opts)
+      if opts.spec == nil then opts.spec = {} end
 
-    config = function()
       local key_descs = {
         c = "Comment",
         C = "Comment with surrounding whitespace",
       }
       for key, desc in pairs(key_descs) do
         for _, prefix in pairs({ "a", "i" }) do
-          require("which-key").add({
+          table.insert(opts.spec, {
             prefix .. key,
             mode = { "o", "x" },
             desc = desc,
@@ -20,6 +20,13 @@ return {
         end
       end
     end,
+  },
+
+  {
+    "glts/vim-textobj-comment",
+    dependencies = "kana/vim-textobj-user",
+
+    event = "ModeChanged",
   },
 
   { import = "plugins.text-objects.various-textobjs" },
