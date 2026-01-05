73dd381a88174fb8143c7c98761187c20588ca53
Mo Hyun <shyun3@yasld.com>
2026-01-05 14:49:31 -0800

    refactor(lualine): move theme to separate module

---
+++ rc/config/nvim/lua/lualine/themes/my_catpuccin.lua
@@ -0,0 +1,25 @@
+-- Derived from molokai in vim-airline-themes
+
+local colors = require("lualine.themes.catppuccin")
+
+--- Create a lualine c section colorscheme whose background lights up with
+--- the mode color if buffer is modified
+---
+---@param mode string Mode name used by lualine theme
+---
+---@return fun(): {fg: string|number, bg: string|number} colorscheme
+local function make_c_section(mode)
+  return function()
+    return vim.bo.modified
+        and { fg = colors[mode].a.fg, bg = colors[mode].a.bg }
+      or colors.normal.c
+  end
+end
+
+local modes = { "normal", "insert", "replace", "visual", "command", "terminal" }
+local override = {}
+for _, mode in ipairs(modes) do
+  override[mode] = { c = make_c_section(mode) }
+end
+
+return vim.tbl_deep_extend("force", colors, override)
--- rc/config/nvim/lua/plugins/lines/lualine.lua
+++ rc/config/nvim/lua/plugins/lines/lualine.lua
@@ -1,31 +1,3 @@
--- Derived from molokai in vim-airline-themes
-local theme = function()
-  local colors = require("lualine.themes.catppuccin")
-
-  --- Create a lualine c section colorscheme whose background lights up with
-  --- the mode color if buffer is modified
-  ---
-  ---@param mode string Mode name used by lualine theme
-  ---
-  ---@return fun(): {fg: string|number, bg: string|number} colorscheme
-  local function make_c_section(mode)
-    return function()
-      return vim.bo.modified
-          and { fg = colors[mode].a.fg, bg = colors[mode].a.bg }
-        or colors.normal.c
-    end
-  end
-
-  local modes =
-    { "normal", "insert", "replace", "visual", "command", "terminal" }
-  local override = {}
-  for _, mode in ipairs(modes) do
-    override[mode] = { c = make_c_section(mode) }
-  end
-
-  return vim.tbl_deep_extend("force", colors, override)
-end
-
 local filename = {
   "filename",
   newfile_status = true,
@@ -68,7 +40,7 @@ return {
 
   opts = {
     options = {
-      theme = theme,
+      theme = "my_catpuccin",
       globalstatus = true,
       refresh = { statusline = 100 },
     },
