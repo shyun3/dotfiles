b03f01d26c03d67662714a042257cf55adcc49a9
Mo Hyun <shyun3@yasld.com>
2026-01-14 13:59:18 -0800

    refactor(vim-endwise): disable filetypes

--- rc/config/nvim/lua/plugins/treesitter/endwise.lua
+++ rc/config/nvim/lua/plugins/treesitter/endwise.lua
@@ -1,1 +1,12 @@
-return { "RRethy/nvim-treesitter-endwise", event = "FileType" }
+return {
+  {
+    LazyDep("vim-endwise"),
+    optional = true,
+
+    opts = {
+      _my_disable_filetypes = { "bash", "lua", "sh", "vim" },
+    },
+  },
+
+  { "RRethy/nvim-treesitter-endwise", event = "FileType" },
+}
