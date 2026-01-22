4e0b913fd2bda82a8557ed5636720f76f52a27ba
Mo Hyun <shyun3@yasld.com>
2026-01-22 12:36:28 -0800

    refactor(lualine): create separate directory

--- rc/config/nvim/lua/plugins/lines/init.lua
+++ rc/config/nvim/lua/plugins/lines/init.lua
@@ -1,9 +1,4 @@
 return {
   { import = "plugins.lines.dropbar" },
-
-  { import = "plugins.lines._noice" },
-  { import = "plugins.lines.lualine" },
-  { import = "plugins.lines.noice" },
-
   { import = "plugins.lines.tabby" },
 }
---
+++ rc/config/nvim/lua/plugins/lualine/init.lua
@@ -0,0 +1,5 @@
+return {
+  { import = "plugins.lualine._noice" },
+  { import = "plugins.lualine.main" },
+  { import = "plugins.lualine.noice" },
+}
