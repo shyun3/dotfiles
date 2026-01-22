fbb39e5567b6372721c3137704e3db69c3bf4d72
Mo Hyun <shyun3@yasld.com>
2026-01-22 15:44:03 -0800

    refactor(snacks): create module

--- rc/config/nvim/lua/plugins/folke/init.lua
+++ rc/config/nvim/lua/plugins/folke/init.lua
@@ -1,34 +1,5 @@
 return {
-  {
-    ---@module 'snacks'
-    "folke/snacks.nvim",
-
-    opts = {
-      lazygit = {
-        win = {
-          style = "lazygit",
-
-          -- Disable <Esc> twice quickly to enter normal mode
-          -- Derived from https://github.com/folke/snacks.nvim/issues/280#issuecomment-2987923844
-          keys = { term_normal = false },
-        },
-      },
-    },
-
-    keys = {
-      {
-        "<Leader>lg",
-        function() Snacks.lazygit() end,
-        desc = "Open lazygit",
-      },
-      {
-        "<Leader>lf",
-        function() Snacks.lazygit.log_file() end,
-        desc = "lazygit: Open log of current file",
-      },
-    },
-  },
-
+  { import = "plugins.folke.snacks" },
   { import = "plugins.folke.lazydev" },
   { import = "plugins.folke.noice" },
   { import = "plugins.folke.which-key" },
---
+++ rc/config/nvim/lua/plugins/folke/snacks.lua
@@ -0,0 +1,30 @@
+---@module 'snacks'
+
+return {
+  "folke/snacks.nvim",
+
+  opts = {
+    lazygit = {
+      win = {
+        style = "lazygit",
+
+        -- Disable <Esc> twice quickly to enter normal mode
+        -- Derived from https://github.com/folke/snacks.nvim/issues/280#issuecomment-2987923844
+        keys = { term_normal = false },
+      },
+    },
+  },
+
+  keys = {
+    {
+      "<Leader>lg",
+      function() Snacks.lazygit() end,
+      desc = "Open lazygit",
+    },
+    {
+      "<Leader>lf",
+      function() Snacks.lazygit.log_file() end,
+      desc = "lazygit: Open log of current file",
+    },
+  },
+}
