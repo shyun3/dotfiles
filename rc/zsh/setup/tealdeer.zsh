07904c3cbf434b3bbe435797f26f8660461b471a
Mo Hyun <shyun3@yasld.com>
2026-03-04 10:49:05 -0800

    fix(tldr): completions not being installed

--- rc/zsh/setup/tealdeer.zsh
+++ rc/zsh/setup/tealdeer.zsh
@@ -1,4 +1,1 @@
-# The tealdeer completion installed through the system package doesn't seem to
-# get picked up by compinit (maybe because the filename doesn't start with an
-# underscore)
-cp /usr/share/zsh/vendor-completions/tldr.zsh $ZSH_CACHE_DIR/completions/_tldr
+cp $ANTIDOTE_CACHE/tealdeer-rs/tealdeer/completion/zsh_tealdeer $ZSH_CACHE_DIR/completions/_tldr
