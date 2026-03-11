8b99b8b2c1f4652f6013b3cd64f4b609e5577891
Mo Hyun <shyun3@yasld.com>
2026-03-11 13:27:04 -0700

    refactor(lspconfig.taplo): remove wildcard root

--- rc/config/nvim/lua/plugins/lsp/lspconfig.lua
+++ rc/config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -82,19 +82,20 @@ return {
         taplo = {
           root_dir = function(bufnr, on_dir)
             local buf_name = vim.api.nvim_buf_get_name(bufnr)
-            local dir_name = vim.fs.dirname(buf_name)
+            local buf_dir_name = vim.fs.dirname(buf_name)
 
             local find_root = require("lspconfig.util").root_pattern(
               -- Taken from lspconfig root markers
               ".taplo.toml",
               "taplo.toml",
-              ".git",
-
-              -- Fallback to enable Taplo on standalone TOML files, see issue
-              -- dotfiles#5
-              "*.toml"
+              ".git"
             )
-            on_dir(find_root(dir_name))
+
+            -- If root isn't found, fallback to current directory. This
+            -- enables Taplo on standalone TOML files, see issue dotfiles#5.
+            local dir = find_root(buf_dir_name) or buf_dir_name
+
+            on_dir(dir)
           end,
         },
       },
