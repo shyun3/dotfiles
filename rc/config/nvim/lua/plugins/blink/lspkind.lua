ee7ba66393ac4d71c7c76c9f753e772b67bceaf2
Mo Hyun <shyun3@yasld.com>
2026-03-26 10:26:12 -0700

    feat(blink): use nvim-web-devicons for kind icon

--- rc/config/nvim/lua/plugins/blink/lspkind.lua
+++ rc/config/nvim/lua/plugins/blink/lspkind.lua
@@ -8,13 +8,30 @@ return {
         menu = {
           draw = {
             components = {
-              -- Derived from https://cmp.saghen.dev/recipes#nvim-web-devicons-lspkind
+              -- Taken from https://cmp.saghen.dev/recipes#icons
+              -- See "nvim-web-devicons + lspkind"
               kind_icon = {
                 text = function(ctx)
-                  return require("lspkind").symbolic(
-                    ctx.kind,
-                    { mode = "symbol" }
-                  ) .. ctx.icon_gap
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, _ =
+                      require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then icon = dev_icon end
+                  else
+                    icon = require("lspkind").symbol_map[ctx.kind] or ""
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, dev_hl =
+                      require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then hl = dev_hl end
+                  end
+                  return hl
                 end,
               },
             },
