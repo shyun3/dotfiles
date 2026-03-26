7621f3d50690ca9e75cb66b8d2214e5e33dad74a
Mo Hyun <shyun3@yasld.com>
2026-03-26 10:07:09 -0700

    fix(zsh): restrict windows terminal settings

--- rc/zsh/pre/winterm.zsh
+++ rc/zsh/pre/winterm.zsh
@@ -1,3 +1,9 @@
+# Detect Windows Terminal
+# Taken from https://stackoverflow.com/q/59733731
+if [[ -z "$WT_SESSION" ]]; then
+    return
+fi
+
 # Make sure Windows Terminal duplicates new WSL tabs/panes in same directory
 # See: https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#zsh
 if [[ $(uname -r) =~ WSL ]]; then
@@ -6,3 +12,9 @@ if [[ $(uname -r) =~ WSL ]]; then
     }
     precmd_functions+=(keep_current_path)
 fi
+
+# Windows Terminal does not currently set COLORTERM, see:
+# https://github.com/microsoft/terminal/issues/11057
+#
+# Some programs use this to detect true color capability
+export COLORTERM=${COLORTERM:-truecolor}
