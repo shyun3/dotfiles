2d86404eae444a83dd2f96133c2249913bcf2455
Mo Hyun <shyun3@yasld.com>
2026-03-30 15:41:09 -0700

    refactor(snacks): split into modules

--- rc/config/nvim/lua/plugins/01-folke/snacks/init.lua
+++ rc/config/nvim/lua/plugins/01-folke/snacks/init.lua
@@ -2,47 +2,6 @@
 
 return {
   {
-    LazyDep("mini.files"),
-    optional = true,
-
-    opts = {
-      _my_rename_callbacks = {
-        -- Taken from rename snack
-        snacks = {
-          desc = "LSP-integrated file rename",
-
-          callback = function(event)
-            Snacks.rename.on_rename_file(event.data.from, event.data.to)
-          end,
-        },
-      },
-    },
-  },
-
-  {
-    LazyDep("oil"),
-    optional = true,
-
-    opts = {
-      _my_post_actions = {
-        -- Taken from rename snack
-        snacks = {
-          desc = "LSP-integrated file rename",
-
-          callback = function(event)
-            local action = event.data.actions[1]
-            if not action then return end
-
-            if action.type == "move" then
-              Snacks.rename.on_rename_file(action.src_url, action.dest_url)
-            end
-          end,
-        },
-      },
-    },
-  },
-
-  {
     LazyDep("snacks"),
 
     -- See `:checkhealth snacks`
