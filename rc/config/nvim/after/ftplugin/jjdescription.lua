a7df637ccf94436712c0dd69769e12dcec872900
Mo Hyun <shyun3@yasld.com>
2026-04-02 10:47:26 -0700

    refactor(ftplugin): move from syntax

--- rc/config/nvim/after/ftplugin/jjdescription.lua
+++ rc/config/nvim/after/ftplugin/jjdescription.lua
@@ -1,2 +1,11 @@
 -- Warn when 1st line of message exceeds 50 characters
 vim.fn.matchadd("@text.danger", [[\v%1l%>50v.+$]])
+
+vim.opt_local.winhighlight:append({
+  diffFile = "my_dim_diffFile",
+  diffIndexLine = "my_dim_diffIndexLine",
+  diffOldFile = "my_dim_diffOldFile",
+  diffNewFile = "my_dim_diffNewFile",
+  diffAdded = "my_dim_diffAdded",
+  diffRemoved = "my_dim_diffRemoved",
+})
--- rc/config/nvim/after/syntax/jjdescription.lua
+++ rc/config/nvim/after/syntax/jjdescription.lua
@@ -1,10 +1,1 @@
-vim.opt_local.winhighlight:append({
-  diffFile = "my_dim_diffFile",
-  diffIndexLine = "my_dim_diffIndexLine",
-  diffOldFile = "my_dim_diffOldFile",
-  diffNewFile = "my_dim_diffNewFile",
-  diffAdded = "my_dim_diffAdded",
-  diffRemoved = "my_dim_diffRemoved",
-})
-
 vim.cmd("syntax sync fromstart")
