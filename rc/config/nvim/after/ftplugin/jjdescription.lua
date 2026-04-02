d0f0ceceb493e659b4dd3db8af4e103b72bfd6c0
Mo Hyun <shyun3@yasld.com>
2026-04-02 15:38:22 -0700

    feat(jjdescription): delete buffer when finished

--- rc/config/nvim/after/ftplugin/jjdescription.lua
+++ rc/config/nvim/after/ftplugin/jjdescription.lua
@@ -1,2 +1,5 @@
+-- These buffers are usually temporary, so delete when done
+vim.bo.bufhidden = "delete"
+
 -- Warn when 1st line of message exceeds 50 characters
 vim.fn.matchadd("@text.danger", [[\v%1l%>50v.+$]])
