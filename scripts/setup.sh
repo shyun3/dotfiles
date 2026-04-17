9f89edfe18edbbac425fd1a563299d42d4d18ec5
Mo Hyun <shyun3@yasld.com>
2026-04-17 11:14:28 -0700

    feat(mise): add ouch
    Replaces atool.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -33,7 +33,7 @@ mkdir -p "$LOCAL_DIR/share/nvim/undo"
 
 #######################################################################
 # Packages
-install_if_missing atool avfs tree universal-ctags xclip xdg-utils zsh
+install_if_missing avfs tree universal-ctags xclip xdg-utils zsh
 
 if [[ $(lsb_release -si 2> /dev/null) == Ubuntu ]]; then
     install_if_missing update-notifier-common
