b488610d55909bb327ec1f0f28f05dd2420cf7b9
Mo Hyun <shyun3@yasld.com>
2026-07-28 14:11:48 -0700

    fix(ratarmount): ensure mount directory exists
    See #33

--- rc/.zsh/plugins/ratarmount.zsh
+++ rc/.zsh/plugins/ratarmount.zsh
@@ -5,12 +5,9 @@ fi
 USER_RUN_DIR="/run/user/$UID"
 RATARMOUNT_VFS="$USER_RUN_DIR/ratarmount"
 if [[ -d $USER_RUN_DIR ]] && [[ -w $USER_RUN_DIR ]]; then
+    mkdir -p "$RATARMOUNT_VFS"
     if ! mountpoint -q "$RATARMOUNT_VFS"; then
-        if [[ ! -e $RATARMOUNT_VFS ]]; then
-            ratarmount --index-file ':memory:' --lazy -r / "$RATARMOUNT_VFS"
-        else
-            print -r -- "Could not create ratarmount VFS at $RATARMOUNT_VFS" >&2
-        fi
+        ratarmount --index-file ':memory:' --lazy -r / "$RATARMOUNT_VFS"
     fi
 else
     print -r -- "ratarmount: Could not access $USER_RUN_DIR" >&2
