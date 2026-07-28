cdc9e61b4494888ae0664532d5b96f0717bab848
Mo Hyun <shyun3@yasld.com>
2026-07-28 14:06:40 -0700

    fix(lint): zshellcheck issues

--- rc/.zsh/plugins/ratarmount.zsh
+++ rc/.zsh/plugins/ratarmount.zsh
@@ -5,15 +5,15 @@ fi
 USER_RUN_DIR="/run/user/$UID"
 RATARMOUNT_VFS="$USER_RUN_DIR/ratarmount"
 if [[ -d $USER_RUN_DIR ]] && [[ -w $USER_RUN_DIR ]]; then
-    if ! mountpoint -q $RATARMOUNT_VFS; then
+    if ! mountpoint -q "$RATARMOUNT_VFS"; then
         if [[ ! -e $RATARMOUNT_VFS ]]; then
-            ratarmount --index-file ':memory:' --lazy -r / $RATARMOUNT_VFS
+            ratarmount --index-file ':memory:' --lazy -r / "$RATARMOUNT_VFS"
         else
-            echo "Could not create ratarmount VFS at $RATARMOUNT_VFS" >&2
+            print -r -- "Could not create ratarmount VFS at $RATARMOUNT_VFS" >&2
         fi
     fi
 else
-    echo "ratarmount: Could not access $USER_RUN_DIR" >&2
+    print -r -- "ratarmount: Could not access $USER_RUN_DIR" >&2
 fi
 
 unset USER_RUN_DIR RATARMOUNT_VFS
