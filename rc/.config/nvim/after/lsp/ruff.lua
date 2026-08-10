f3e63f1b923a006c87df1afa3f316b6bdc5a9587
Mo Hyun <shyun3@yasld.com>
2026-08-10 09:18:51 -0700

    refactor(ruff): lsp capabilities

--- rc/.config/nvim/after/lsp/ruff.lua
+++ rc/.config/nvim/after/lsp/ruff.lua
@@ -5,9 +5,9 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 --
 -- Enable it to add support for dynamic configuration
 -- See https://docs.astral.sh/ruff/editors/features/#dynamic-configuration
-capabilities.workspace = capabilities.workspace or {}
-capabilities.workspace.didChangeWatchedFiles = {
-  dynamicRegistration = true,
-}
+capabilities.workspace =
+  vim.tbl_deep_extend("force", capabilities.workspace or {}, {
+    didChangeWatchedFiles = { dynamicRegistration = true },
+  })
 
 return { capabilities = capabilities }
