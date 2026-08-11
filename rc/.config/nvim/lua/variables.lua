28350511e9db2a677e676d8d2901bc147a78aeae
Mo Hyun <shyun3@yasld.com>
2026-08-11 15:21:06 -0700

    deps(mise): use wl-clipboard
    Replaces `xclip`. Has simpler syntax.

--- rc/.config/nvim/lua/variables.lua
+++ rc/.config/nvim/lua/variables.lua
@@ -4,24 +4,24 @@ vim.g.c_no_curly_error = 1
 
 -- Clipboard
 -- Derived from https://github.com/neovim/neovim/issues/10223#issuecomment-703544303
-if vim.fn.exists("$DISPLAY") then
+if vim.fn.exists("$WAYLAND_DISPLAY") then
   vim.g.clipboard = {
-    name = "xclip with ^M Trim",
+    name = "wl-clipboard with ^M Trim",
     copy = {
-      ["+"] = "xclip -i -sel clip",
-      ["*"] = "xclip -i",
+      ["+"] = "wl-copy --type text/plain",
+      ["*"] = "wl-copy --type text/plain --primary",
     },
     paste = {
       ["+"] = function()
         return vim.fn.systemlist(
-          'xclip -o -sel clip -t UTF8_STRING 2>/dev/null | sed -e "s/\r$//"',
+          'wl-paste --no-newline --type "text/plain;charset=utf-8" 2>/dev/null | sed -e "s/\r$//"',
           "",
           1
         )
       end,
       ["*"] = function()
         return vim.fn.systemlist(
-          'xclip -o -t UTF8_STRING 2>/dev/null | sed -e "s/\r$//"',
+          'wl-paste --no-newline --type "text/plain;charset=utf-8" --primary 2>/dev/null | sed -e "s/\r$//"',
           "",
           1
         )
