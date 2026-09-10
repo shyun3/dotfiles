aa313c4ae37095299a7ba9af17d75a6c433fb8ef
Mo Hyun <shyun3@yasld.com>
2026-09-10 12:08:23 -0700

    fix(ratarmount): check for empty mountpoint
    See #33

--- rc/.zsh/plugins/ratarmount.zsh
+++ rc/.zsh/plugins/ratarmount.zsh
@@ -7,7 +7,11 @@ RATARMOUNT_VFS="$USER_RUN_DIR/ratarmount"
 if [[ -d $USER_RUN_DIR ]] && [[ -w $USER_RUN_DIR ]]; then
     mkdir -p "$RATARMOUNT_VFS"
     if ! mountpoint -q "$RATARMOUNT_VFS"; then
-        ratarmount --index-file ':memory:' --lazy --debug 0 -r / "$RATARMOUNT_VFS"
+        if [[ -z "$(ls -A $RATARMOUNT_VFS)" ]]; then
+            ratarmount --index-file ':memory:' --lazy --debug 0 -r / "$RATARMOUNT_VFS"
+        else
+            print -r -u 2 -- "ratarmount: $RATARMOUNT_VFS was non-empty"
+        fi
     fi
 else
     print -r -u 2 -- "ratarmount: Could not access $USER_RUN_DIR"
