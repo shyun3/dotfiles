38cb0704abcfdbc527baf3af24137f77477d2271
Mo Hyun <shyun3@yasld.com>
2026-09-11 11:16:34 -0700

    docs(ratarmount): add issue reference

--- rc/.zsh/plugins/ratarmount.zsh
+++ rc/.zsh/plugins/ratarmount.zsh
@@ -4,17 +4,19 @@ fi
 
 USER_RUN_DIR="/run/user/$UID"
 RATARMOUNT_VFS="$USER_RUN_DIR/ratarmount"
+
+# See dotfiles#33 for all these checks
 if [[ -d $USER_RUN_DIR ]] && [[ -w $USER_RUN_DIR ]]; then
     mkdir -p "$RATARMOUNT_VFS"
     if ! mountpoint -q "$RATARMOUNT_VFS"; then
         if [[ -z "$(ls -A $RATARMOUNT_VFS)" ]]; then
             ratarmount --index-file ':memory:' --lazy --debug 0 -r / "$RATARMOUNT_VFS"
         else
-            print -r -u 2 -- "ratarmount: $RATARMOUNT_VFS was non-empty"
+            print -r -u 2 "ratarmount: $RATARMOUNT_VFS was non-empty"
         fi
     fi
 else
-    print -r -u 2 -- "ratarmount: Could not access $USER_RUN_DIR"
+    print -r -u 2 "ratarmount: Could not access $USER_RUN_DIR"
 fi
 
 unset USER_RUN_DIR RATARMOUNT_VFS
